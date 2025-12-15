e6f081596aa9c908386c380848c5427d10e9a3a1
Apple <apple@Apples-MacBook-Air.local>
2025-12-15 16:52:25 +0100

    Configure Vercel for Express backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ app.get("/api/health", (req, res) => {
 if(ENV.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../admin/dist')));
 
-  app.get('/{*any}', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../admin', 'dist', 'index.html'));
   });
 }
