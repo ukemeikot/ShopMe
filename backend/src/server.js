40d8951ecc17011127123bbb744aade032debdf9
Apple <apple@Apples-MacBook-Air.local>
2025-12-15 13:04:33 +0100

    Initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+app.get("/api/health", (req, res) => {
+  res.send("OK the server is working now!!!");
+});
