8533737e911a754ef22f39c8cea9f14cacc297df
Apple <apple@Apples-MacBook-Air.local>
2025-12-15 15:06:38 +0100

    Deployment check

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import path from 'path';
+import { ENV } from './config/env.js';
+
+const __dirname = path.resolve();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
 app.get("/api/health", (req, res) => {
   res.send("OK the server is working now!!!");
+});
+
+//make the app ready for deployment
+if(ENV.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../admin/dist')));
+
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../admin', 'dist', 'index.html'));
+  });
+}
+
+app.listen(ENV.PORT, () => {
+  console.log('Server is running on port 3000');
 });
